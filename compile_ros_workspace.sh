2efd4a77c9d316b0fd472568d20697ce4ea8ff10
nicolov <nicolov@users.noreply.github.com>
2019-07-22 00:39:18 +0000

    Upgrade to ROS melodic on Bionic

--- compile_ros_workspace.sh
+++ compile_ros_workspace.sh
@@ -12,8 +12,9 @@ rosinstall_generator \
     --deps \
     --tar \
     --flat \
+    --rosdistro melodic \
     $PACKAGES > ws.rosinstall
-wstool init -j1 src ws.rosinstall
+wstool init -j8 src ws.rosinstall
 
 catkin config \
     --install \
